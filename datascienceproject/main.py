cd298c9aadf17b385f1b0c7631861a0f64fcdd8a
Muhammad Derylian Khalfani <mderylkhalfani@gmail.com>
2025-07-28 23:33:49 +0700

    Model Trainer

--- datascienceproject/main.py
+++ datascienceproject/main.py
@@ -2,6 +2,7 @@ from src.datascience import logger
 from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.datascience.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 from src.datascience.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
+from src.datascience.pipeline.model_trainer_pipeline import ModelTrainerTrainingPipeline
 
 STAGE_NAME="Data Ingestion Stage"
 
@@ -27,10 +28,20 @@ except Exception as e:
 
 STAGE_NAME = "Data Transformation stage"
 try:
-    logger.info("Starting {}".format(STAGE_NAME))
-    data_validation = DataTransformationTrainingPipeline()
-    data_validation.initiate_data_transformation()
-    logger.info(f"{STAGE_NAME}  COMPLETED")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   data_transformation = DataTransformationTrainingPipeline()
+   data_transformation.initiate_data_transformation()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
-    logger.exception(e)
-    raise e
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Model Trainer stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   data_transformation = ModelTrainerTrainingPipeline()
+   data_transformation.initiate_model_training()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
